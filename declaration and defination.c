3c611a2a2b508c47a93d9c856da57d3f9924a3b7
DARSHANM-C <darshanmc100@gmail.com>
2024-11-27 12:45:26 +0530

    Create declaration and defination.c

---
+++ declaration and defination.c
@@ -0,0 +1,11 @@
+"function declaration
+int multiply(intx,inty);
+int main(){
+intresult=multiply(45);
+printf("product=%d/n",result);
+return0;
+}
+"function dinition
+int multiply(intx.inty){
+reurnx*y;
+}
