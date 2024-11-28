f7134fe8aec83b9c1f2a4a8c9480b567110e0a90
DARSHANM-C <darshanmc100@gmail.com>
2024-11-28 12:05:19 +0530

    Create Matrix addition.c

---
+++ Matrix addition.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(){
+int rows cols;
+printf("enter the number of rows and columns,");
+scan f ("%d,"frows,&cols);
+inta[rows][cols],b[rows][cols]sum[rows][cols];
+print("enter elements of materix a:/n");
+for (int i=0i<rows;i++){
+for (intj=0,i<cols;j++){
+scanf("%d,"&a[i][j]);
+ }
+}
+printf("enter element of mateix b:\n");
+for (int i=0:i<rows;i++){
+printf("%d",sum[i][j]);
+}
