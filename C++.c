f9e9c544503c503d85c54c1ec2b75bd1f6c7e507
DARSHANM-C <darshanmc100@gmail.com>
2024-11-29 15:02:22 +0530

    Create C++.c

---
+++ C++.c
@@ -0,0 +1,14 @@
+Else: Check Positive, Negative, or Zero
+
+#include <stdio.h>
+int main() {
+    int num;
+    printf("Enter a number: ");
+    scanf("%d", &num);
+    if (num > 0)
+        printf("The number is positive.\n");
+    else if (num < 0)
+        printf("The number is negative.\n");
+    else
+        printf("The number is zero.\n");
+    return 0;
