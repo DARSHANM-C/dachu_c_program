2c7df5a0df3091b2046a49a62b7a0f2e838cd5ea
DARSHANM-C <darshanmc100@gmail.com>
2024-11-29 14:46:23 +0530

    Create Book.c

---
+++ Book.c
@@ -0,0 +1,16 @@
+include<stdio.h>
+struct bookdetsils{
+ int bookId;
+ char bookmame[50]
+ char authorname[50]
+ int price:
+}
+int main()
+struct bookdetails b1;
+print("enter book details");
+printf("enter book id");
+scanf("%d,%b1,id bookdetails);
+printf("enter author name");
+scanf("%s",A.author);
+printf("Book:%s\nprice
+     :%d\nAuthor:%s\n, A.book, A
